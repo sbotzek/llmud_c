f8b1af51f3d6940a236475086b82903b5793100b
Steven B <steven@botzek.com>
2025-04-20 22:00:27 -0700

    Add CHECK and CHECK_MSG
    This is for asserting conditions

--- include/common.h
+++ include/common.h
@@ -1,6 +1,29 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define UNUSED(x) (void)(x)
+
+#define CHECK(cond) \
+    do { \
+        if (!(cond)) { \
+            fprintf(stderr, "[CHECK FAILED]\n"); \
+            fprintf(stderr, "  Condition: %s\n", #cond); \
+            fprintf(stderr, "  Location : %s:%d\n", __FILE__, __LINE__); \
+            abort(); \
+        } \
+    } while (0)
+
+#define CHECK_MSG(cond, msg) \
+    do { \
+        if (!(cond)) { \
+            fprintf(stderr, "[CHECK FAILED] %s\n", (msg)); \
+            fprintf(stderr, "  Condition: %s\n", #cond); \
+            fprintf(stderr, "  Location : %s:%d\n", __FILE__, __LINE__); \
+            abort(); \
+        } \
+    } while (0)
 
 #endif // COMMON_H
